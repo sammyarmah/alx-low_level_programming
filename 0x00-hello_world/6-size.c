beffa88168efe836ba4f0e44740729d2b23ed84f
Samue Armah <samuelarmah817@gmail.com>
2023-02-16 08:33:50 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the size of various on a computer
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float f;
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(j));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+printf("Size of long long int:%lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
