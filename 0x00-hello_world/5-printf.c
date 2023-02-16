8f21cde780ef7a7f4f04a3ef17b407a1d8120a1c
Samue Armah <samuelarmah817@gmail.com>
2023-02-16 08:09:44 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - Printing a line using the printf function
+ * main - Printing a line using printf function
  * Return: 0
  */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
