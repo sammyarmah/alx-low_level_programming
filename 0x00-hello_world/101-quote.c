ab8fd0a3a5471d1b254dbd32d82eb89532de5b46
Samue Armah <samuelarmah817@gmail.com>
2023-02-16 10:44:12 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
+
 /**
- * main - A c program that prints a line to a standard error
- * Return 1 (Sucess)
+ * main - prints exactly and that piece of art is useful
+ *
+ * Return Always 0 (Sucess)
  */
 int main(void)
+
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
 }
