ef931f0073e5b7e0d94b8e3c1b423fc293d8cf49
Samue Armah <samuelarmah817@gmail.com>
2023-02-16 09:10:03 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+ * main - A c program that prints a line to a standard error
+ * Return 1 (Sucess)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
