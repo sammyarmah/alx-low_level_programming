ddb84288e9f1583ea718829d1a1c45dd78faa053
Samue Armah <samuelarmah817@gmail.com>
2023-02-21 13:59:51 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int abs(int);
-int _print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
 void table_table(void);
 int add(int, int);
