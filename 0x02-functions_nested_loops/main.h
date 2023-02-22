08ecb8c0fb0392241f092dac32047dfb443f33ef
Samue Armah <samuelarmah817@gmail.com>
2023-02-21 16:09:10 -0800

    updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int print_sign(int n);
 int abs(int);
 int print_last_digit(int n);
 void jack_bauer(void);
-void table_table(void);
+void times_table(void);
 int add(int, int);
 void _print_to_98(int n);
 void print_times_table(int n);
