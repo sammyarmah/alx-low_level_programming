16fd130ec532cf4bbee282f2b82ce2d3fd0d6532
Samue Armah <samuelarmah817@gmail.com>
2023-02-21 18:39:35 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * Returns: 0
+ */
+int main(void)
+{
+	int count
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib2_
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit
- * @n: the number
+ * @n: the number to be treated
  * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int last;
 
-	if (ld  < 0)
-		ld *= -1;
-
-	_putchar(ld + '0');
-
-	return (0);
+	last = n % 10;
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
