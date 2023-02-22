55671c245b261a9c21a012855ffd28a87aaf86f6
Samue Armah <samuelarmah817@gmail.com>
2023-02-21 17:02:10 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
  * @n: The number to start printing from
  * Return: Always 0
  */
-void _print_to_98(void)
+void print_to_98(void)
 {
 	if (n <= 98)
 	{
@@ -14,7 +14,7 @@ void _print_to_98(void)
 	if (n == 98)
 	{
 	printf("%d", n);
-	printf('\n');
+	printf("\n");
 	break;
 	}
 	else
@@ -30,7 +30,7 @@ void _print_to_98(void)
 	if (n == 98)
 	{
 	printf("%d", n);
-	printf('\n');
+	printf("\n");
 	break;
 	}
 	else
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,5 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void _print_to_98(int n);
+void print_to_98(int n);
 void print_times_table(int n);
