950564034648565046ee51b7618612dcf773a6e0
Samue Armah <samuelarmah817@gmail.com>
2023-02-23 01:45:21 -0800

    2-largest_number.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -6,19 +7,16 @@
 */
 
 int main(void)
+
 {
         int i;
 
         printf("Infinite loop incoming :(\n");
-
         i = 0;
-
         /*while (i < 10)*/
         /*{*/
         /*        putchar(i);*/
         /*}*/
-
         printf("Infinite loop avoided! \\o/\n");
-
         return (0);
 }
