12df6b654f729d0b4fac71d42ca8cc8a3c8def1c
Samue Armah <samuelarmah817@gmail.com>
2023-02-23 16:25:39 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,19 @@
 int main(void)
 
 {
+	
 	int x;
 
 	for (x = 1; x <= 100; x++)
 	{
-	if ((x % 3 ==0) && (x % 5 == 0))
+	if ((x % 3 == 0) && (x % 5 == 0))
 	{
 	printf("FizzBuzz");
 	}
 	else if (x % 3 == 0)
 	{
 	printf("Fizz");
+	}
 	else if (x % 5 == 0)
 	{
 	printf("Buzz");
@@ -36,6 +38,4 @@ int main(void)
 	}
 	printf("\n");
 	return (0);
-}
-
-			
+}	
