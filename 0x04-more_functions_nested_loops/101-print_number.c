2cb8533fa5f1fa2fbdb72a6ea69fd8968f4fd182
Samue Armah <samuelarmah817@gmail.com>
2023-02-23 17:11:58 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,16 +11,16 @@ void print_number(int n)
 
 {
 
-	unsigned int g = n;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
 	n *= -1;
-	g = n;
+	k = n;
 	_putchar('-');
 	}
-	g /= 10;
-	if (g != 0)
+	k /= 10;
+	if (k != 0)
 	print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
 }
