91838e0ef0279ff19c824e430e759ea6b582a5d7
Samue Armah <samuelarmah817@gmail.com>
2023-02-27 13:07:22 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * puts_half -  a function that prints half of a string, followed by a new line
+ * @str: string
+ * Return: 
+ */
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_array - a function that prints n elements of an array of integers, followed by a new line.
+ * @a: array name
+ * @n: is the number of the array to be printed
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+		printf("\n");
+}
