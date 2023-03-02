dd2cd45dd3c272b243804896bc39757d2bf9e728
Samue Armah <samuelarmah817@gmail.com>
2023-03-02 08:20:00 -0800

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev - string  - reverse array
+ * @n: integer
+ * Return: Always 0.
+ */
+
+void rev_string (char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++; i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
