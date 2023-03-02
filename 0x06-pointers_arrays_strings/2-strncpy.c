d805bd38581572346240505b7242b02b72ccfd6d
Samue Armah <samuelarmah817@gmail.com>
2023-03-02 05:29:03 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,6 @@
  * @n: input value
  * Return: dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
@@ -20,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	while (j < n)
 	{
-		dest[j] != '\0';
+		dest[j] = '\0';
 		j++;
 	}
 	return (dest);
