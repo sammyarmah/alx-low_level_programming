896fdc33089ba064c3fa9a12d79202121048d478
Samue Armah <samuelarmah817@gmail.com>
2023-03-14 16:19:36 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,6 +21,12 @@ int **alloc_grid(int width, int height)
 
 	if (a == NULL)
 	{
+		free(a);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
 		a[i] = malloc(sizeof(int) * width);
 
 		if (a[i] == NULL)
