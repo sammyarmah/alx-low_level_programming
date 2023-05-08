d6ed048004e71dcde0b3faf9159da384d5ccf2fb
Samuel Armah <samuelarmah817@gmail.com>
2023-05-08 16:13:32 -0700

    main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filenmae, size_t letters);
+int _strlen(char *str);
+
+#endif
