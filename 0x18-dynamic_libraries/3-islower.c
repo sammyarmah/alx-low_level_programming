fab62def4dc7ec4c3ef5757c502d122c2bb77015
Samuel Armah <samuelarmah817@gmail.com>
2023-06-19 16:14:47 -0700

    3-islower.c

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character.
+ * @c: input to be checked.
+ *
+ * Return: 1 if lower, else 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+
