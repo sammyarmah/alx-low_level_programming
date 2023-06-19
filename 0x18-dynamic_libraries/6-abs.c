0f3c7a108da5c9bbd68ea13fe9ed35a67e858af8
Samuel Armah <samuelarmah817@gmail.com>
2023-06-19 16:26:57 -0700

    6-abs.c

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer.
+ *
+ *@a: positive or negative integer supplied.
+ *
+ * Return: Always 0
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-a);
+	}
+}
+
